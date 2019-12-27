3acbbf3dcdcbb6aa79e21f6fc7ce6e592f0e0a51
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-27 16:31:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
 
-variable "key_name" {
-  description = "Private key name to use with instance"
-  default     = "terraform"
-}
 
 variable "instance_type" {
   description = "AWS instance type"
@@ -11,14 +7,12 @@ variable "instance_type" {
 
 variable "ami" {
   description = "Base AMI to launch the instances"
-
-  # Bitnami NGINX AMI
+ # Bitnami NGINX AMI
   default = "ami-0dacb0c129b49f529"
 }
 
 variable "subnet_id" {
   description = "Instance to be launched"
-
-  # Bitnami NGINX AMI
+# Bitnami NGINX AMI
   default = "subnet-8716fbec"
 }
