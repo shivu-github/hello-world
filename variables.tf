f0fd199f2b2f53a28680a74cd9e2bebe32cb0aef
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-28 13:20:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,12 +7,15 @@ variable "instance_type" {
 
 variable "ami" {
   description = "Base AMI to launch the instances"
- # Bitnami NGINX AMI
   default = "ami-0dacb0c129b49f529"
 }
 
 variable "subnet_id" {
   description = "Instance to be launched"
-# Bitnami NGINX AMI
   default = "subnet-8716fbec"
+}
+
+variable "instance_ips" {
+  description = "IP"
+  default = ""
 }
