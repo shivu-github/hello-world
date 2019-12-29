0b7e327a60a814c3b7393ee9b9c367eeb0c09f4d
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-29 22:02:58 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,36 +1,17 @@
-terraform {
-  backend "s3" {
-    bucket = "shivu-new"
-    key    = "shivu"
-    region = "us-east-2"
-    access_key = "AKIAQSPKRYWR54HSMOV7"
-    secret_key = "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"
-  }
-}
-
-# Use AWS Terraform provider
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIAQSPKRYWR54HSMOV7"
-  secret_key = "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"
-
+  access_key = "AKIAUY5V4M2ZIFFVNTMC"
+  secret_key = "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"
 }
 
 # Create EC2 instance
-resource "aws_instance" "shivuec2" {
-  subnet_id              = "${var.subnet_id}"   
-  ami                    = "${var.ami}"
-  #count                  = "${var.count}"
-  #key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.shivusg.id}"]
-  source_dest_check      = false
-  instance_type          = "${var.instance_type}"
-
+resource "aws_instance" "newawsec2" {
+  ami                    = "ami-0dacb0c129b49f529"
+  instance_type          = "t2.micro"
 }
 
-# Create Security Group for EC2
 resource "aws_security_group" "shivusg" {
-  name = "shivu-servian-sg"
+  name = "shivusg"
 
   ingress {
     from_port   = 80
