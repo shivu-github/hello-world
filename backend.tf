b472f69b49cf4902865397d853492aab6397cf6c
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-30 15:28:47 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "shivu-ec2"
+    key    = "shivu-servian"
+    region = "us-east-2"
+    access_key = "${AWS_ACCESS_KEY}"
+    secret_key = "${AWS_SECRET_KEY}"
+  
+}
