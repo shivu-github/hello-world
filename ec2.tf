7e529f0de2fa041110353f4914a2475fa6aad0ad
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-30 15:37:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "${AWS_ACCESS_KEY}"
-  secret_key = "${AWS_SECRET_KEY}"
+  access_key = AWS_ACCESS_KEY
+  secret_key = AWS_SECRET_KEY
 }
 
 # Create EC2 instance
