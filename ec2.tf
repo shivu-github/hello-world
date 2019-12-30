d2cf7e167f92e2daa58fa5de4c51a119a0bbb94a
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-30 17:52:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -43,6 +43,13 @@ resource "aws_security_group" "servian-sg5" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
