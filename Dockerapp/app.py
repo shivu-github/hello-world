c3e142a2983dcb8a405e4e54c5bfd51899b0443f
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-30 21:56:48 +0530

    Update app.py

--- Dockerapp/app.py
+++ Dockerapp/app.py
@@ -1,14 +1,27 @@
+import time
+import redis
 from flask import Flask
 from redis import Redis
 
-app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+app=Flask(__name__)
+cache=redis.Redis(host='redis',port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0 :
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
-
+        count = get_hit_count()
+        return 'Hello World! You have hit {} times.\n'.format(count)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",debug=True)
+			
