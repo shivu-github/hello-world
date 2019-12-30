266eacba0f0fde91b575f93515f3bc0605ae13a0
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-30 15:27:07 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "shivu-ec2"
+    key    = "shivu-servian"
+    region = "us-east-2"
+    access_key = "${AWS_ACCESS_KEY}"
+    secret_key = "${AWS_SECRET_KEY}"
+  
+}
+
 provider "aws" {
   region = "us-east-2"
   access_key = "${AWS_ACCESS_KEY}"
