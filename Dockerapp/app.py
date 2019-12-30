7a941bcd507bd0c0a3dbc0c31e0370c147461253
shivu-github <56473817+shivu-github@users.noreply.github.com>
2019-12-30 18:49:25 +0530

    Update app.py

--- Dockerapp/app.py
+++ Dockerapp/app.py
@@ -7,14 +7,14 @@ cache=redis.Redis(host='redis',port=6379)
 
 def get_hit_count():
     retries = 5
-        while True:
-          try:
+    while True:
+        try:
             return cache.incr('hits')
-          except redis.exceptions.ConnectionError as exc:
+        except redis.exceptions.ConnectionError as exc:
             if retries == 0 :
-              raise exc
-          retries -= 1
-          time.sleep(0.5)
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello():
@@ -23,3 +23,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
+			
