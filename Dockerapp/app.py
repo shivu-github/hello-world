a46064bac129560fe0e1c33f7706e223b7b5994f
shivu-github <56473817+shivu-github@users.noreply.github.com>
2020-01-02 13:25:06 +0530

    Update app.py

--- Dockerapp/app.py
+++ Dockerapp/app.py
@@ -21,7 +21,7 @@ def get_hit_count():
 @app.route('/')
 def hit():
     count = get_hit_count()
-    return 'I have been hit %i times since deployment.\n' % int(count)
+    return 'Server has been hit %i times since deployment.\n' % int(count)
 
 
 if __name__ == "__main__":
