606c124e6fe91ec0a56b77cf937958f3d4f79e89
shivu-github <56473817+shivu-github@users.noreply.github.com>
2020-01-02 13:00:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "servian-ec35" {
   ami                    = "ami-0dacb0c129b49f529"
   instance_type          = "t2.micro"
   key_name               = "shivu-key"
-  vpc_security_group_ids = ["${aws_security_group.servian-sg5.id}"]
+  vpc_security_group_ids = ["${aws_security_group.servian-sg35.id}"]
   user_data = <<EOF
                 #!/bin/bash
                 sudo yum update -y
@@ -20,8 +20,8 @@ resource "aws_instance" "servian-ec35" {
          }
 }
 
-resource "aws_security_group" "servian-sg5" {
-  name = "servian-sg5"
+resource "aws_security_group" "servian-sg35" {
+  name = "servian-sg35"
 
   ingress {
     from_port   = 80
